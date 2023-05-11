31d289f38f0821972842397e21c7a8dd0278d15e
Kevin Lee Cardoza <49277166+Kiboot@users.noreply.github.com>
2023-05-11 16:33:36 +0800

    player code added

--- Player.cs
+++ Player.cs
@@ -3,37 +3,26 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	double moveSpeed = 100;
+	double jumpForce = 200;
+	double gravity = 500;
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y += (float)(gravity * delta);
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
+		velocity.X = 0;
+
+		if(Input.IsKeyPressed(Key.Left)){
+			velocity.X -= (float)moveSpeed;
+		}
+		if(Input.IsKeyPressed(Key.Right)){
+			velocity.X = (float)moveSpeed;
+		}
+		
 	}
 }
